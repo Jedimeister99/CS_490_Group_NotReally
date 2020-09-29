40ab0b094bc4c997188e12e83da5f86ed5436a1f
Hannah Szmurlo <szmurlh@sunypoly.edu>
2020-09-28 20:17:35 -0400

    Attempt to change extraction script - file reading

--- keras/faceswap-master/scripts/ckimages.py
+++ keras/faceswap-master/scripts/ckimages.py
@@ -26,7 +26,7 @@ def main ():
         #sequence level?
         #print(subject)
         for sequence in (os.listdir(os.path.join(sourceDir, subject))):
-            #print(sequence)
+            print(sequence)
             for file in sequence:
                 if file.endswith(".png"):
                     imagePaths.append(os.path.join(sourceDir,subject,sequence,file))
