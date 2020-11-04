6838311cd363b81e407579d17fe23e5a7ac7405d
Hannah Szmurlo <szmurlh@sunypoly.edu>
2020-11-04 18:20:34 -0500

    working on file grabbing

--- keras/faceswap-master/lib/utils.py
+++ keras/faceswap-master/lib/utils.py
@@ -196,9 +196,7 @@ def get_image_paths(directory):
     #allFiles = glob.glob(directory, recursive=True)
 
     #dir_scanned = sorted(os.scandir(directory), key=lambda x: x.name)
-    dir_scanned = glob.glob('directory/*.png', recursive = True)
-    for f in glob.glob('/home/szmurlh/subjects/S080/*.png'):
-        print("Checking")
+    dir_scanned = glob.glob('directory\*.png', recursive = True)
     #print(dir_scanned)
     #print(type(dir_scanned[0]))
     #print(type(allFiles[0]))
