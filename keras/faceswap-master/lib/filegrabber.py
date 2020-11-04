f10e4631ab0373cf3da6fdba95b5ad8dc476cf3d
Hannah Szmurlo <szmurlh@sunypoly.edu>
2020-11-04 16:03:31 -0500

    working on file grabbing

---
+++ keras/faceswap-master/lib/filegrabber.py
@@ -0,0 +1,4 @@
+#!/usr/bin python3
+""" Main entry point to the training process of FaceSwap """
+
+import os
--- keras/faceswap-master/lib/model/filegrabber.py
+++
@@ -1,6 +0,0 @@
-#read in subject folder
-#scan through subject folder and dump files into a temp directory
-#call train or convert
-#delete temp directory
-
-
